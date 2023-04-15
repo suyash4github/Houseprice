0e4a7584961dc8134878befe90c74b9c3af84a1b
suyash4 <sarlimar4@gmail.com>
2023-04-15 14:01:20 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,8 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
-from data_transformation import DataTransformationConfig
-from data_transformation import DataTransformation
+
 
 @dataclass
 class DataIngestionCongig:
@@ -45,9 +44,4 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    data_transformation.initaite_data_transformation(train_data,test_data)
